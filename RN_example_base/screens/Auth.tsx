3dc92200ffb27d36b114fee96c66170d5438efe2
Dat Tran <dat.tran@adamodigital.com>
2021-07-11 17:33:27 +0700

    add firebase auth

--- RN_example_base/screens/Auth.tsx
+++ RN_example_base/screens/Auth.tsx
@@ -1,36 +1,94 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import auth from '@react-native-firebase/auth';
+// import { AppleButton } from '@invertase/react-native-apple-authentication';
 
 export function Auth() {
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
 
-  // Handle user state changes
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing) setInitializing(false);
-  }
-
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
 
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    console.log(user);
+
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  const _anonymous = () => {
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        console.log('User signed in anonymously');
+      })
+      .catch(error => {
+        if (error.code === 'auth/operation-not-allowed') {
+          console.log('Enable anonymous in your firebase console.');
+        }
+
+        console.error(error);
+      });
+  };
+
+  const _loginWithEmailAndPassword = () => {
+    auth()
+      .createUserWithEmailAndPassword(
+        'jane.doe@example.com',
+        'SuperSecretPassword!',
+      )
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  };
+
+  const _signOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
   if (initializing) return null;
 
-  if (!user) {
+  if (!!user) {
     return (
-      <View>
-        <Text>Login</Text>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Login</Text>
       </View>
     );
   }
 
   return (
-    <View>
-      <Text>Welcome {user.email}</Text>
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Welcome {user?.email}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
